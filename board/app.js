faae2573b73498d377298d6ec1f14243536c42db
newExpand <magin0auto@gmail.com>
2023-08-17 00:50:26 +0900

    리스트 UI 만들기

--- board/app.js
+++ board/app.js
@@ -7,7 +7,7 @@ app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-    res.render("home", { title: "안녕하세요", message: "만나서 반갑습니다." });
+    res.render("home", { title: "테스트 게시판", message: "만나서 반갑습니다." });
 });
 
 app.listen(8080);

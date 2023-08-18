342a2b740952cdc64e49e8c91f7b415ba9706081
newExpand <magin0auto@gmail.com>
2023-08-18 21:16:51 +0900

    리스트 api 작성(페이지네이션 기능 추가)

--- board/app.js
+++ board/app.js
@@ -17,8 +17,17 @@ app.engine(
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => {
-    res.render("home", { title: "테스트 게시판", message: "만나서 반갑습니다." });
+app.get("/", async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const search = req.query.search || "";
+
+    try {
+        const [posts, paginator] = await postService.list(collection, page, search);
+        res.render("home", { title: "테스트 게시판", search, paginator, posts });
+    } catch (err) {
+        console.error(err);
+        res.render("home", { title: "테스트 게시판" });
+    }
 });
 
 app.get("/write", (req, res) => {
